c20e62601e352d49d049c9f8db05dcc2991631a6
Alexander1-svg <al.bellod@duocuc.cl>
2026-05-18 15:15:09 -0400

    feat: Insert datos

--- db/init.sql
+++ db/init.sql
@@ -9,5 +9,12 @@ CREATE TABLE IF NOT EXISTS productos (
     stock INT NOT NULL
 );
 
+INSERT INTO productos (nombre, descripcion, precio, stock) VALUES
+('Alimento Cachorro Premium', 'Sabor a pollo, razas pequenas', 19990, 15),
+('Alimento Adulto Light', 'Control de peso, razas medianas', 17990, 8),
+('Snacks Dentales', 'Ayuda a la limpieza dental', 5990, 30),
+('Alimento Adulto Pedigree', 'Sabor carne', 15990, 40),
+('Bravery pollo Adulto raza pequena', 'Sabor a pollo', 25990, 20);
+
 -- Innovatech Database Initialization Script
 -- Version 1.1.0
